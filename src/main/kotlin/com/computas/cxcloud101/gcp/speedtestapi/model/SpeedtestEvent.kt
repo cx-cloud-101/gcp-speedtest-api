618fa3b359bec7efe4de99ff8bc043fb7fb970bc
Alexander Hoem Rosbach <alexander.hoem.rosbach@computas.com>
2018-10-16 13:23:19 +0200

    Add type to SpeedtestEvent.data

--- src/main/kotlin/com/computas/cxcloud101/gcp/speedtestapi/model/SpeedtestEvent.kt
+++ src/main/kotlin/com/computas/cxcloud101/gcp/speedtestapi/model/SpeedtestEvent.kt
@@ -6,5 +6,34 @@ import java.io.Serializable
 data class SpeedtestEvent @JsonCreator constructor(
         val user: String,
         val device: Int,
-        val data: String,
+        val data: Result,
         val timestamp: Long) : Serializable
+
+data class Result @JsonCreator constructor(
+        val speeds: Speeds,
+        val client: Client,
+        val server: Server
+) : Serializable
+
+data class Speeds @JsonCreator constructor(
+        val download: Number,
+        val upload: Number
+) : Serializable
+
+data class Client @JsonCreator constructor(
+        val ip: String,
+        val lat: Number,
+        val lon: Number,
+        val isp: String,
+        val country: String
+) : Serializable
+
+data class Server @JsonCreator constructor(
+        val host: String,
+        val lat: Number,
+        val lon: Number,
+        val country: String,
+        val distance: Number,
+        val ping: Number,
+        val id: String
+) : Serializable
